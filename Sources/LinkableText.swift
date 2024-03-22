5ee49ed7504605b69a6f8db694b3f03ddb0d04af
Shinya Kumagai <roomful.rooms@gmail.com>
2024-03-22 18:23:27 +0900

    Update

--- Sources/LinkableText.swift
+++ Sources/LinkableText.swift
@@ -32,6 +32,44 @@ struct LinkableText: View {
     }
 }
 
+struct LinkableMarkdownText: View {
+    private let markdown: AttributedString
+
+    init(_ markdown: String, underlined: Bool = true) {
+        self.markdown = makeAttributedString(
+            from: markdown,
+            underlined: underlined
+        )
+    }
+
+    var body: some View {
+        Text(markdown)
+    }
+}
+
+private func makeAttributedString(
+    from markdown: String,
+    underlined: Bool
+) -> AttributedString {
+    var dest: AttributedString
+    do {
+        // ref. https://zenn.dev/link/comments/40d2073e63d41c
+        let options = AttributedString.MarkdownParsingOptions(
+            allowsExtendedAttributes: true,
+            interpretedSyntax: .inlineOnlyPreservingWhitespace,
+            appliesSourcePositionAttributes: true
+        )
+        dest = try AttributedString(markdown: markdown, options: options)
+    } catch {
+        return AttributedString(markdown)
+    }
+
+    for run in dest.runs where run.link != nil {
+        dest[run.range].underlineStyle = underlined ? .single : .none
+    }
+    return dest
+}
+
 #Preview(traits: .sizeThatFitsLayout) {
     VStack(alignment: .leading, spacing: 8.0) {
         LinkableText("Click here.", links: [
@@ -40,6 +78,16 @@ struct LinkableText: View {
         LinkableText("詳しくはこちら", links: [
             "こちら": URL(string: "https://www.example.com")!,
         ])
+
+        LinkableMarkdownText(
+            "Click [here](https://www.example.com).",
+            underlined: true
+        )
+
+        LinkableMarkdownText(
+            "詳しくは[こちら](https://www.example.com).",
+            underlined: true
+        )
     }
     .tint(.red)
     .padding()
